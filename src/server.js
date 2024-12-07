2272a5c5c1d0308e9686f9fd2ff13e7be03b5419
gabriel ribeiro <gabrielsribeiro9999@gmail.com>
2024-12-07 19:57:51 -0300

    midl deposit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,67 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid")
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+const customers = [];
 
-app.get("/", (req, res) => {
-    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+function veryfyExistsAccountCpf (req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({ error: "Customer not found"  })
+    };
+
+    req.customer = customer;
+
+    return next();    
+}
+
+app.post("/account", (req, res) => {
+    const { name, cpf } = req.body;
+
+    const customersAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customersAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists!" })
+    } 
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidV4(),
+        statement: []
+    });
+
+    return res.status(201).json({ mensage: "Sucsses" })
 });
+
+app.get("/statement", veryfyExistsAccountCpf, (req, res) => {
+    const { customer } = req;
+    return res.json( customer.statement );
+});
+
+app.post("/deposit", veryfyExistsAccountCpf ,(req, res) => {
+    const {description, amount} = req.body;
+
+    const { customer } = req;
+    
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).json({ sucsses: "Sucesso ao inserir" })
+})
 
 app.listen(3333, () => console.log("http://localhost:3333"));
